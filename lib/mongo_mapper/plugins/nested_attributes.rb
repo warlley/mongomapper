eb43552c195e062597d3fe7e1775b23c5dcb79fe
Warlley <warlleyrezende@gmail.com>
2010-03-25 14:40:33 -0300

    Included embedded documents validations

--- lib/mongo_mapper/plugins/nested_attributes.rb
+++ lib/mongo_mapper/plugins/nested_attributes.rb
@@ -43,71 +43,76 @@ module MongoMapper
   #    def _destroy
   #      marked_for_destruction?
   #    end
+  
+      module InstanceMethods
 
-      private
+        private
 
-      UNASSIGNABLE_KEYS = %w( id _id _destroy )
+        UNASSIGNABLE_KEYS = %w( id _id _destroy )
 
-      def assign_nested_attributes_for_collection_association(association_name, attributes_collection)
-        options = nested_attributes_options[association_name]
+        def assign_nested_attributes_for_collection_association(association_name, attributes_collection)
+          options = nested_attributes_options[association_name]
 
-        unless attributes_collection.is_a?(Hash) || attributes_collection.is_a?(Array)
-          raise ArgumentError, "Hash or Array expected, got #{attributes_collection.class.name} (#{attributes_collection.inspect})"
-        end
+          unless attributes_collection.is_a?(Hash) || attributes_collection.is_a?(Array)
+            raise ArgumentError, "Hash or Array expected, got #{attributes_collection.class.name} (#{attributes_collection.inspect})"
+          end
 
-        if options[:limit] && attributes_collection.size > options[:limit]
-          raise TooManyDocuments, "Maximum #{options[:limit]} documents are allowed. Got #{attributes_collection.size} documents instead."
-        end
+          if options[:limit] && attributes_collection.size > options[:limit]
+            raise TooManyDocuments, "Maximum #{options[:limit]} documents are allowed. Got #{attributes_collection.size} documents instead."
+          end
 
-        if attributes_collection.is_a? Hash
-          attributes_collection = attributes_collection.sort_by { |index, _| index.to_i }.map { |_, attributes| attributes }
-        end
+          if attributes_collection.is_a? Hash
+            attributes_collection = attributes_collection.sort_by { |index, _| index.to_i }.map { |_, attributes| attributes }
+          end
 
-        attributes_collection.each do |attributes|
-          attributes = attributes.with_indifferent_access
+          attributes_collection.each do |attributes|
+            attributes = attributes.with_indifferent_access
 
-          if attributes['id'].blank?
-            unless reject_new_record?(association_name, attributes)
-              send(association_name).build(attributes.except(*UNASSIGNABLE_KEYS))
+            if attributes['id'].blank?
+              unless reject_new_record?(association_name, attributes)
+                send(association_name).build(attributes.except(*UNASSIGNABLE_KEYS))
+              end
+            elsif existing_document = send(association_name).detect { |document| document.id.to_s == attributes['id'].to_s }
+              assign_to_or_mark_for_destruction(association_name, existing_document, attributes, options[:allow_destroy])
+            else
+              raise_nested_attributes_document_not_found(association_name, attributes['id'])
             end
-          elsif existing_document = send(association_name).detect { |document| document.id.to_s == attributes['id'].to_s }
-            assign_to_or_mark_for_destruction(association_name, existing_document, attributes, options[:allow_destroy])
-          else
-            raise_nested_attributes_document_not_found(association_name, attributes['id'])
           end
         end
-      end
 
-      def assign_to_or_mark_for_destruction(association_name, document, attributes, allow_destroy)
-        if has_destroy_flag?(attributes) && allow_destroy
-          send(association_name).delete_if {|q| q.id.to_s == document.id.to_s }
-        else
-          document.attributes = attributes.except(*UNASSIGNABLE_KEYS)
-          #document.update_attributes(attributes.except(*UNASSIGNABLE_KEYS))
+        def assign_to_or_mark_for_destruction(association_name, document, attributes, allow_destroy)
+          if has_destroy_flag?(attributes) && allow_destroy
+            send(association_name).delete_if {|q| q.id.to_s == document.id.to_s }
+          else
+            document.attributes = attributes.except(*UNASSIGNABLE_KEYS)
+            #document.update_attributes(attributes.except(*UNASSIGNABLE_KEYS))
+          end
         end
-      end
 
-      def has_destroy_flag?(hash)
-        Boolean.to_mongo(hash['_destroy'])
-      end
-
-      def reject_new_record?(association_name, attributes)
-        has_destroy_flag?(attributes) || call_reject_if(association_name, attributes)
-      end
-
-      def call_reject_if(association_name, attributes)
-        case callback = nested_attributes_options[association_name][:reject_if]
-        when Symbol
-          method(callback).arity == 0 ? send(callback) : send(callback, attributes)
-        when Proc
-          nested_keys = attributes.keys.select {|k| k.to_s =~ /attributes$/}
-          callback.call(attributes.except(*nested_keys).except(*UNASSIGNABLE_KEYS))
+        def has_destroy_flag?(hash)
+          Boolean.to_mongo(hash['_destroy'])
         end
-      end
 
-      def raise_nested_attributes_document_not_found(association_name, document_id)
-        raise DocumentNotFound, "Couldn't find #{association_name.classify} with ID=#{document_id} for #{self.class.name} with ID=#{id}"
-       end
+        def reject_new_record?(association_name, attributes)
+          has_destroy_flag?(attributes) || call_reject_if(association_name, attributes)
+        end
+
+        def call_reject_if(association_name, attributes)
+          case callback = nested_attributes_options[association_name][:reject_if]
+          when Symbol
+            method(callback).arity == 0 ? send(callback) : send(callback, attributes)
+          when Proc
+            nested_keys = attributes.keys.select {|k| k.to_s =~ /attributes$/}
+            callback.call(attributes.except(*nested_keys).except(*UNASSIGNABLE_KEYS))
+          end
+        end
+
+        def raise_nested_attributes_document_not_found(association_name, document_id)
+          raise DocumentNotFound, "Couldn't find #{association_name.classify} with ID=#{document_id} for #{self.class.name} with ID=#{id}"
+        end
+      
+      end
+       
     end
   end
 end
